eb9d4ca98cb7257d398fc71f293c0c0a44133a4d
Deepak Baravkar <deepaksbaravkar98@gmail.com>
2022-08-01 15:52:35 +0530

    MVC Day 1

---
+++ MVC_Day1/MVC_Day1/Controllers/DemoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Day1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Day1.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string NormalMethod()
+        {
+            return "Hello.. This is normal method view!";
+        }
+        public ContentResult Contents()
+        {
+            return Content("This is ContentResult method view");
+        }
+        [NonAction]
+        public EmptyResult NoResult()
+        {
+            int amount = 30000;
+            float SI = (amount * 3 * 2);
+            return new EmptyResult();
+        }
+        public ViewResult ViewResultMethod()
+        {
+            ViewBag.Data = "This is ViewResult method of Demo controller";
+            return View();
+        }
+        public JsonResult EmployeeData()
+        {
+            Employee e = new Employee();
+            e.ID = 1;
+            e.Name = "Deepak";
+            e.Age = 24;
+            return Json(e);
+        }
+
+        public RedirectResult RedirectMethod()
+        {
+            return Redirect("Contents");
+        }
+
+        public RedirectResult RedirectToPrivacy()
+        {
+            return Redirect("/Home/Privacy");
+        }
+    }
+}
---
+++ MVC_Day1/MVC_Day1/Controllers/HrController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Day1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Day1.Controllers
+{
+    public class HrController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> depList = new List<Department>()
+            {
+                new Department{ID=1, Name="IT"},
+                new Department{ID=2, Name="Account"},
+                new Department{ID=3, Name="Maintenance"},
+
+            };
+
+            return View("ListDepartment",depList);
+        }
+        public ActionResult ListDepartment(List<Department> dlist)
+        {
+            return View(dlist);
+        }
+
+        public ActionResult ListEmployee()
+        {
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee{ID=1,Name="Deepak",Age=24},
+                new Employee{ID=2,Name="Raj",Age=24},
+                new Employee{ID=3,Name="Shiv",Age=24},
+                new Employee{ID=4,Name="Vikas",Age=24},
+                new Employee{ID=5,Name="Sagar",Age=24},
+            };
+            return View(emplist);
+        }
+
+        public ActionResult DisplayEmployee()
+        {
+            Employee e = new Employee()
+            {
+                ID = 1,
+                Name = "Deepak",
+                Age = 24,
+            };
+            return View(e);
+        }
+    }
+}
