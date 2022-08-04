f61797afb9681e99897908ced5ccea87008ccdf8
Deepak Baravkar <deepaksbaravkar98@gmail.com>
2022-08-04 10:53:17 +0530

    Assignment 3 in progress

---
+++ WebApp_Core_EF/WebApp_Core_EF/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_Core_EF.Models;
+using System.Data;
+using System.Web.Mvc;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using ValidateAntiForgeryTokenAttribute = Microsoft.AspNetCore.Mvc.ValidateAntiForgeryTokenAttribute;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+using BindAttribute = System.Web.Mvc.BindAttribute;
+using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
+using System.Net;
+
+namespace WebApp_Core_EF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository emprepo;
+
+        public HomeController(ILogger<HomeController> logger, IEmployeeRepository erep)
+        {
+            _logger = logger;
+            emprepo = erep;
+        }
+
+       // [ActionName("AllEmp")]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AllEmp()
+        {
+            var model = emprepo.GetAllEmployees();
+            return View(model);
+        }
+
+        public IActionResult AddEmp(Employee employee)
+        {
+            var model = emprepo.AddEmployee(employee);
+            return View(model);
+        }
+        public IActionResult Create(Employee employee)
+        {
+            var model = emprepo.AddEmployee(employee);
+           // return RedirectToAction("Index");
+           return View(model);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Eit,Name,Gender,Salary,Address")] Employee employee)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        emprepo.AddEmployee(employee);
+
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(employee);
+        //}
+
+
+
+
+    }
+}
---
+++ WebApp_Core_EF/WebApp_Core_EF/Models/EmployeeRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_Core_EF.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDBContext db;
+        public EmployeeRepository(EmployeeDBContext context)
+        {
+            db = context;
+        }
+
+        //Implementation codes
+        public Employee AddEmployee(Employee employee)
+        {
+             db.Employees.Add(employee);
+            db.SaveChanges();
+            return employee;
+        }
+
+        public Employee DeleteEmployee(int Id)
+        {
+            Employee emp = db.Employees.Find(Id);
+            if (emp != null)
+            {
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                
+            }
+            return emp;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+
+            return db.Employees;
+        }
+
+        public Employee GetEmployeeByID(int id)
+        {
+            return db.Employees.Find(id);
+        }
+
+        public Employee UpdateEmployee(Employee employeechanges)
+        {
+            var employee = db.Employees.Attach(employeechanges);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return employeechanges;
+
+            //Alternatively we can check individual property state
+
+            //db.Employees.Attach(employeechanges);
+            //var entry = db.Entry(employeechanges);
+            //entry.Property(e => e.Name).IsModified = true;
+            ////same way for all properties
+            //entry.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+    }
+}
