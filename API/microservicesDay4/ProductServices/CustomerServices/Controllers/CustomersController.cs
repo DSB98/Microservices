5dfe20cb52a6148045511fdc65dbe66cd0373a67
Deepak Baravkar <deepaksbaravkar98@gmail.com>
2022-08-04 11:46:47 +0530

    Microservices Handson

---
+++ API/microservicesDay4/ProductServices/CustomerServices/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET: api/<CustomersController>
+        [HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        public IEnumerable<CustomerModel> Get()
+        {
+            CustomerModel customerModel1 = new CustomerModel();
+            CustomerModel customerModel2 = new CustomerModel();
+
+            customerModel1.CustomerID = 1;
+            customerModel1.CustomerName = "AK";
+            customerModel1.CustomerPhone = 1111;
+
+            customerModel2.CustomerID=2;
+            customerModel2.CustomerName = "DJ";
+            customerModel2.CustomerPhone = 222;
+
+            List<CustomerModel> customerList = new List<CustomerModel>();
+            customerList.Add(customerModel1);
+            customerList.Add(customerModel2);
+
+            return customerList;
+
+
+
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/microservicesDay4/ProductServices/ProductServices/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        public IEnumerable<ProductModel> Get()
+        {
+            ProductModel productModel1 = new ProductModel();
+            ProductModel productModel2 = new ProductModel();
+            productModel1.ProductId = 1;
+            productModel1.ProductName = "LAptop";
+            productModel1.Description = "Dell";
+            productModel1.Price = 56000;
+
+            productModel2.ProductId = 2;
+            productModel2.ProductName = "Mobile";
+            productModel2.Description = "Oneplus";
+            productModel2.Price = 40000;
+
+            List<ProductModel> productList = new List<ProductModel>();
+            productList.Add(productModel1);
+            productList.Add(productModel2);
+            return productList;
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
